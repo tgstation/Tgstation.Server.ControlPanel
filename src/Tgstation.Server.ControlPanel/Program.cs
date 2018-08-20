98cfbe20a7295a5b5fbe4b22ed586f185d5a3ed9
Cyberboss <Cyberboss@users.noreply.github.com>
2018-08-20 16:41:57 -0400

    Strip cruff

--- src/Tgstation.Server.ControlPanel/Program.cs
+++ src/Tgstation.Server.ControlPanel/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Tgstation.Server.ControlPanel
 {
+	/// <summary>
+	/// Contains the entrypoint for the application
+	/// </summary>
 	static class Program
 	{
 		/// <summary>
@@ -14,9 +13,6 @@ namespace Tgstation.Server.ControlPanel
 		[STAThread]
 		static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
 		}
 	}
 }
