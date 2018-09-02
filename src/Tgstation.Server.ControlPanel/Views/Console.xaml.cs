e60b49c75575e40772fdc49548ff1fbb10b18a26
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-09-02 14:03:28 -0400

    Add auto scrolling to request console

--- src/Tgstation.Server.ControlPanel/Views/Console.xaml.cs
+++ src/Tgstation.Server.ControlPanel/Views/Console.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using System;
 
 namespace Tgstation.Server.ControlPanel.Views
 {
@@ -14,6 +16,17 @@ namespace Tgstation.Server.ControlPanel.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
+
+			var scrollViewer = this.FindControl<ScrollViewer>("_scrollViewer");
+			DispatcherTimer timer = new DispatcherTimer
+			{
+				Interval = TimeSpan.FromSeconds(1)
+			};
+			timer.Tick += ((sender, e) =>
+			{
+				scrollViewer.Offset = new Vector(0, scrollViewer.Extent.Height);
+			});
+			timer.Start();
+		}
     }
 }
