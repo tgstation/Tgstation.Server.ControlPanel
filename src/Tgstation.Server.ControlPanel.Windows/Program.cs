e75be7fbdb436dd72586fe073f439e3b49715aff
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-08 14:40:26 -0400

    Try to re-enable squirrelling [TGSDeploy]

--- src/Tgstation.Server.ControlPanel.Windows/Program.cs
+++ src/Tgstation.Server.ControlPanel.Windows/Program.cs
@@ -5,7 +5,7 @@ namespace Tgstation.Server.ControlPanel.Windows
 {
 	class Program : IUrlEncoder
 	{
-		public static void Main() => ControlPanel.Run(new Program(), new NoopUpdater());
+		public static void Main() => ControlPanel.Run(new Program(), new Updater());
 		public static AppBuilder BuildAvaloniaApp() => ControlPanel.BuildAvaloniaApp();
 
 		public string UrlEncode(string input) => HttpUtility.UrlEncode(input);
