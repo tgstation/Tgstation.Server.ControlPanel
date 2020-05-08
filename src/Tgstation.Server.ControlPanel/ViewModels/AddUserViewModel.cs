7b81f4da661bd8d7d7c91f4b2b8593880063309c
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-08 10:38:27 -0400

    Fix needing 1 more character than the minimum password length

--- src/Tgstation.Server.ControlPanel/ViewModels/AddUserViewModel.cs
+++ src/Tgstation.Server.ControlPanel/ViewModels/AddUserViewModel.cs
@@ -107,7 +107,7 @@ namespace Tgstation.Server.ControlPanel.ViewModels
 				case AddUserCommand.Close:
 					return true;
 				case AddUserCommand.Add:
-					return ((Username.Length > 0 && Password.Length > serverInformation.MinimumPasswordLength) ^ (SystemIdentifier.Length > 0)) && Password == ConfirmPassword;
+					return ((Username.Length > 0 && Password.Length >= serverInformation.MinimumPasswordLength) ^ (SystemIdentifier.Length > 0)) && Password == ConfirmPassword;
 				default:
 					throw new ArgumentOutOfRangeException(nameof(command), command, "Invalid command!");
 			}
