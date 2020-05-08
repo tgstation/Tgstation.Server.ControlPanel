863d92da2fca3b918bbd88b79c9188c86a9c8159
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-08 14:18:31 -0400

    Fix BYOND version display in compile jobs list

--- src/Tgstation.Server.ControlPanel/ViewModels/CompileJobViewModel.cs
+++ src/Tgstation.Server.ControlPanel/ViewModels/CompileJobViewModel.cs
@@ -18,6 +18,7 @@ namespace Tgstation.Server.ControlPanel.ViewModels
 		public string Duration => (CompileJob.Job.StoppedAt.Value - CompileJob.Job.StartedAt.Value).ToString("hh\\:mm\\:ss");
 
 		public string Revision => CompileJob.RevisionInformation.CommitSha.Substring(0, 7);
+		public string ByondVersion => $"{CompileJob.ByondVersion.Major}.{CompileJob.ByondVersion.Minor}";
 		public string DMApiVersion => $"{CompileJob.DMApiVersion.Major}.{CompileJob.DMApiVersion.Minor}.{CompileJob.DMApiVersion.Build}";
 		public string OriginRevision => CompileJob.RevisionInformation.OriginCommitSha.Substring(0, 7);
 
