ab00cba9882d4e01bc1db471719456cd007efe78
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-08 20:43:36 -0400

    This was better

---
+++ src/Tgstation.Server.ControlPanel.Windows/NoopUpdater.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tgstation.Server.ControlPanel.Windows
+{
+	sealed class NoopUpdater : IUpdater
+	{
+		public bool Functional => false;
+
+		public Task ApplyUpdate(Action<int> progress) => throw new NotSupportedException();
+
+		public void Dispose() { }
+
+		public Task<Version> LatestVersion(Action<int> progress) => throw new NotSupportedException();
+
+		public void RestartApp() => throw new NotSupportedException();
+	}
+}
