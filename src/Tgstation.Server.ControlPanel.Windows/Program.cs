7ddca14def9b1c893e0ae9ca189f7d5af684a348
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-19 17:03:12 -0400

    Fix TLS for lower .NET versions on windows

--- src/Tgstation.Server.ControlPanel.Windows/Program.cs
+++ src/Tgstation.Server.ControlPanel.Windows/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using System.Net;
 using System.Web;
 
 namespace Tgstation.Server.ControlPanel.Windows
@@ -6,7 +7,17 @@ namespace Tgstation.Server.ControlPanel.Windows
 	class Program : IUrlEncoder
 	{
 		public static void Main() => ControlPanel.Run(new Program(), new NoopUpdater());
-		public static AppBuilder BuildAvaloniaApp() => ControlPanel.BuildAvaloniaApp();
+		public static AppBuilder BuildAvaloniaApp()
+		{
+			// Only apply when current setting is not SystemDefault (0) added in .NET 4.7
+			if ((int)ServicePointManager.SecurityProtocol != 0)
+			{
+				// Add Tls1.2 to the existing enabled protocols
+				ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+			}
+
+			return ControlPanel.BuildAvaloniaApp();
+		}
 
 		public string UrlEncode(string input) => HttpUtility.UrlEncode(input);
 	}
