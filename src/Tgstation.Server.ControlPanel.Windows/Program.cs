d6799f2a0726488c3ecf513f36317fd03fec7600
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-23 10:18:23 -0400

    Fix TLS

--- src/Tgstation.Server.ControlPanel.Windows/Program.cs
+++ src/Tgstation.Server.ControlPanel.Windows/Program.cs
@@ -9,12 +9,8 @@ namespace Tgstation.Server.ControlPanel.Windows
 		public static void Main() => ControlPanel.Run(new Program(), new NoopUpdater());
 		public static AppBuilder BuildAvaloniaApp()
 		{
-			// Only apply when current setting is not SystemDefault (0) added in .NET 4.7
-			if ((int)ServicePointManager.SecurityProtocol != 0)
-			{
-				// Add Tls1.2 to the existing enabled protocols
-				ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
-			}
+			// Add Tls1.2 to the existing enabled protocols
+			ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
 
 			return ControlPanel.BuildAvaloniaApp();
 		}
