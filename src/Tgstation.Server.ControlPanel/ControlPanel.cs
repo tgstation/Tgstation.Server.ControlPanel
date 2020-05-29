c09cae57306d7581e76fa1fb5df53a3de683ebbb
Jordan Brown <Cyberboss@users.noreply.github.com>
2020-05-28 23:29:33 -0400

    Startup fixes

--- src/Tgstation.Server.ControlPanel/ControlPanel.cs
+++ src/Tgstation.Server.ControlPanel/ControlPanel.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Logging.Serilog;
 using System.Diagnostics;
+using System.Linq;
+using System.Net;
 using Tgstation.Server.ControlPanel.ViewModels;
 using Tgstation.Server.ControlPanel.Views;
 
@@ -8,10 +10,21 @@ namespace Tgstation.Server.ControlPanel
 {
 	public static class ControlPanel
 	{
-		public static void Run(IUpdater updater)
+		public static void Run(IUpdater updater, string[] args)
 		{
+			// Add Tls1.2 to the existing enabled protocols
+			ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+
 			using var mwvm = new MainWindowViewModel(updater);
-			var app = BuildAvaloniaApp();
+			AppBuilder app;
+			if (args.FirstOrDefault().ToUpperInvariant() == "--WIN32-HACK")
+				app = AppBuilder.Configure<App>()
+				.UseWin32()
+				.UseDirect2D1()
+				.UseReactiveUI()
+				.LogToDebug();
+			else
+				app = BuildAvaloniaApp();
 
 			app.BeforeStarting(x => mwvm.AsyncStart());
 			app.Start<MainWindow>(() => mwvm);
--- src/Tgstation.Server.ControlPanel/Program.cs
+++ src/Tgstation.Server.ControlPanel/Program.cs
@@ -6,13 +6,6 @@ namespace Tgstation.Server.ControlPanel
 {
 	static class Program
 	{
-		public static void Main() => ControlPanel.Run(new NoopUpdater());
-		public static AppBuilder BuildAvaloniaApp()
-		{
-			// Add Tls1.2 to the existing enabled protocols
-			ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
-
-			return ControlPanel.BuildAvaloniaApp();
-		}
+		public static void Main(string[] args) => ControlPanel.Run(new NoopUpdater(), args);
 	}
 }
